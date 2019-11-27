34b12d089dc3184ae50b98606966a2a113bd70b8
d.rogozhny@gmail.com <d.rogozhny@gmail.com>
2019-11-28 00:29:20 +0300

    initial commit

---
+++ footer/collab-footer/README.md
@@ -0,0 +1,3 @@
+## SharePoint Starter Kit Collaboration Footer
+
+The collaboration footer from the [sp-starter-kit](https://github.com/SharePoint/sp-starter-kit) moved to a separate package.
---
+++ footer/collab-footer/src/extensions/collabFooter/loc/myStrings.d.ts
@@ -0,0 +1,12 @@
+declare interface ICollabFooterApplicationCustomizerStrings {
+  Title: string;
+  MyLinks: string;
+  EditMyLinks: string;
+  MyLinksSaveSuccess: string;
+  MyLinksSaveFailed: string;
+}
+
+declare module 'CollabFooterApplicationCustomizerStrings' {
+  const strings: ICollabFooterApplicationCustomizerStrings;
+  export = strings;
+}
